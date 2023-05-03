4a8c8f79292f173a0f9eea2c7d988809b2572b90
Avikmahato <128340287+Avikmahato@users.noreply.github.com>
2023-05-03 10:08:23 +0530

    Add files via upload

---
+++ Marksheet.py
@@ -0,0 +1,22 @@
+marks=[]
+total=0
+for x in range(0,5):
+    a=int(input("Enter Mark :"))
+    marks.insert(x,a)
+    total=marks[x]+total
+per=total/5
+for x in range(0,5):
+    print("Subject(",x+1,")",'=',marks[x])
+print("Total Marks   :",total)
+print("Percentage    :",per,"%")
+if per>=90 :
+    print("Grade         :A")
+elif per>=80 and per<90:
+    print("Grade         :B")
+elif per>=70 and per<80:
+    print("Grade         :C")
+elif per>=60 and per<70:
+    print("Grade         :D")
+else:
+    print("Fail")
+
